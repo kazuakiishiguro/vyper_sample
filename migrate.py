612d4c78ea4b6a8602838ae9dc8d6907562f176d
Kazuaki Ishiguro <kazuaki@couger.co.jp>
2018-12-14 09:29:27 +0900

    changed migrate script

--- migrate.py
+++ migrate.py
@@ -1,4 +1,8 @@
 import os 
 
-os.system('vyper -f "abi" contracts/ERC20.vy > abi/ERC20.json')
-os.system('vyper -f "bytecode" contracts/ERC20.vy > bytecode/ERC20.txt')
+# TODO create function that detects files from ./contracts directory
+contracts = ['ERC20', 'nameregistry']
+
+for contract in contracts:
+    os.system('vyper -f "abi" contracts/{0}.vy > abi/{0}.json'.format(contract))
+    os.system('vyper -f "bytecode" contracts/{0}.vy > bytecode/{0}.txt'.format(contract))
